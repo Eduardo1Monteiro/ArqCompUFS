1b08ce520d2e9e1331df14fb41b1eba656bfbc99
Eduardo1Monteiro <eduardomonteiro0047@gmail.com>
2025-06-10 02:14:01 -0300

    feat: loadMem

--- projeto1/eduardomonteiro202400051102.c
+++ projeto1/eduardomonteiro202400051102.c
@@ -21,6 +21,36 @@ Files readFile(char *argv[]) {
   return files;
 }
 
+void loadMemory(FILE *input, uint32_t offset, uint8_t *mem) {
+
+  char *lineBuffer = (char *)(malloc(256 * sizeof(char)));
+  uint32_t instruction;
+  uint32_t currentAddress = offset;
+
+  rewind(input);
+
+  while (fgets(lineBuffer, 256, input) != NULL) {
+
+    uint32_t memIndex = currentAddress - offset;
+
+    sscanf(lineBuffer, "%x", &instruction);
+
+    for (uint32_t i = 0; i < 4; i++) {
+      mem[memIndex + i] = (instruction >> (8 * i)) & 0xFF;
+    }
+
+    currentAddress += 4;
+  }
+
+  for (uint32_t i = 0; i < 12; i++) {
+    printf("mem[%i] = %02x\n", i, mem[i]);
+  }
+
+  free(lineBuffer);
+
+  return;
+}
+
 int main(int argc, char *argv[]) {
 
   printf("---------------------------------------------------------------------"
@@ -45,6 +75,8 @@ int main(int argc, char *argv[]) {
       "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
   uint8_t *mem = (uint8_t *)(malloc(32 * 1024));
+
+  loadMemory(files.input, offset, mem);
 
   return 0;
 }
