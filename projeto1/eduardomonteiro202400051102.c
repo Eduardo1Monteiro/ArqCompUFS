3c457826a4dc1d14d8f00e381d6889a514735a6c
Eduardo1Monteiro <eduardomonteiro0047@gmail.com>
2025-06-10 01:29:07 -0300

    memory initialized

--- projeto1/eduardomonteiro202400051102.c
+++ projeto1/eduardomonteiro202400051102.c
@@ -33,5 +33,18 @@ int main(int argc, char *argv[]) {
 
   Files files = readFile(argv);
 
+  const uint32_t offset = 0x80000000;
+
+  uint32_t pc = offset;
+
+  uint32_t x[32] = {0};
+
+  const char *x_label[32] = {
+      "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+      "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+      "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+
+  uint8_t *mem = (uint8_t *)(malloc(32 * 1024));
+
   return 0;
 }
